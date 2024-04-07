e3dbb0e5be97ac1ebf8c2368b7e936214754bd77
Anton <yushkevich.a.a@yandex.ru>
2024-04-07 22:08:27 +0300

    1

--- src/background/BGWrapper.tsx
+++ src/background/BGWrapper.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
-import { BGRounds } from "./BGRounds";
 
 const Wrapper = styled.div`
 	position: relative;
@@ -15,14 +14,14 @@ const Content = styled.div`
 	width: 100%;
 	height: 100%;
 	background-color: rgba(22, 26, 29, 0.4);
-	backdrop-filter: blur(300px);
 `;
 
 export const BGWrapper = ({ children }: { children: ReactNode }) => {
 	return (
 		<Wrapper>
-			<BGRounds />
-			<Content>{children}</Content>;
+			<Content>
+				{children}
+			</Content>;
 		</Wrapper>
 	);
 };
