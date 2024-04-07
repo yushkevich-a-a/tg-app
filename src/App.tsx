05a68eac9b8b0cbef2082f87d0ad255f879c6610
Anton <yushkevich.a.a@yandex.ru>
2024-04-08 00:37:12 +0300

    раскрытие на весь экран

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 import { Header } from "./components/Header";
 import { Body } from "./components/Body";
+import { useTelegram } from "./hooks/useTelegram";
 
 export const App = () => {
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.expand();
+  }, []);
+
   return (
     <ThemeProvider>
       <div>
